e7e1fc91a40f42add2157bf3ecfbfec2a902ecbb
Bowie <bowie.leidy@gmail.com>
2018-08-27 15:56:19 -0400

    pushing changes to the pid functionality to remove obfuscated code being carried in from other cookie values.

--- app/src/javascript/mlhelpers/_setPid.js
+++ app/src/javascript/mlhelpers/_setPid.js
@@ -5,17 +5,27 @@ var setPid = function() {
         lae_eg = getValue('lae_eg'),
         ml_acc = getValue('ml_acc'),
         lae_vid = getValue('lae_vid'),
+        pidTime = today.getTime(),
+        ENVisitCommercialScore = getCookie('ENVisitCommercialScore'),
+        PageVisitCommercialScore = getCookie('PageVisitCommercialScore'),
         pidParam, pid;
 
 
     if (pidBase === undefined) {
         pidBase = "";
+    }
+    if (pidBase != undefined) {
+        if (ENVisitCommercialScore.indexOf(pidBase) != -1 || (PageVisitCommercialScore.indexOf(pidBase) != -1)) {
+            var tempPid = getParameterByName(pidBase, ENVisitCommercialScore);
+            tempPid = tempPid.substring(0, tempPid.indexOf('-'));
+            pid = tempPid;
+        }
     } else {
         pid = pidBase;
     }
 
 
-    pidParam = '-pid-' + pid + '-ml_count-' + ml_count + '-ml_acc-' + ml_acc + '-ml_eg-' + ml_eg + '-lae_eg-' + lae_eg + '-lae_vid-' + lae_vid;
+    pidParam = '-pid-' + pid + '-ml_count-' + ml_count + '-ml_acc-' + ml_acc + '-ml_eg-' + ml_eg + '-lae_eg-' + lae_eg + '-lae_vid-' + lae_vid + '-time-' + pidTime;
 
 
     if (getCookie('pid') !== pidParam) {
