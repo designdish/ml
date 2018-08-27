ca92d1ca7bb1741f0548796e9e3895ebebbd3d71
Bowie <bowie.leidy@gmail.com>
2018-08-27 16:36:51 -0400

    quick change

--- app/src/javascript/bundle.js
+++ app/src/javascript/bundle.js
@@ -7,64 +7,60 @@ var buildUrl = function(cdn, file) {
     return url;
 };
 
+var loadBundle =
 
-// load helper functions
+    // load helper functions
+    Promise.all([
+        load.js(buildUrl(helperCDN, "_addEvent.js")),
+        load.js(buildUrl(helperCDN, "_splitArray.js")),
+        load.js(buildUrl(helperCDN, "_isEqual.js")),
+        load.js(buildUrl(helperCDN, "_samePageNavigation.js")),
+        load.js(buildUrl(helperCDN, "_getClosest.js")),
+        console.log("global helpers loaded @ " + today)
+    ]).then(
 
-var loadHelpers = Promise.all([
-    load.js(buildUrl(helperCDN, "_addEvent.js")),
-    load.js(buildUrl(helperCDN, "_splitArray.js")),
-    load.js(buildUrl(helperCDN, "_isEqual.js")),
-    load.js(buildUrl(helperCDN, "_samePageNavigation.js")),
-    load.js(buildUrl(helperCDN, "_getClosest.js")),
-    console.log("global helpers loaded @ " + today)
-]);
+        // load parameter functions
+        Promise.all([
+            load.js(buildUrl(helperCDN, "_appendParam.js")),
+            load.js(buildUrl(helperCDN, "_appendParamValues.js")),
+            load.js(buildUrl(helperCDN, "_checkParams.js")),
+            load.js(buildUrl(helperCDN, "_compareParams.js")),
+            load.js(buildUrl(helperCDN, "_joinParams.js")),
+            load.js(buildUrl(helperCDN, "_updateJoinedParams.js")),
+            load.js(buildUrl(helperCDN, "_updateParams.js")),
+            console.log("param helpers loaded @ " + today)
+        ])).then(
 
+        // load cookie functions
+        Promise.all([
+            load.js(buildUrl(helperCDN, "_getCookie.js")),
+            load.js(buildUrl(helperCDN, "_eraseCookie.js")),
+            load.js(buildUrl(helperCDN, "_logCookie.js")),
+            load.js(buildUrl(helperCDN, "_setCookie.js")),
+            load.js(buildUrl(helperCDN, "_updateCookie.js")),
+            load.js(buildUrl(helperCDN, "_syncCookies.js")),
+            console.log("cookie helpers loaded @ " + today)
+        ])).then(
 
-//load parameter functions
-var loadParams = Promise.all([
-    load.js(buildUrl(helperCDN, "_appendParam.js")),
-    load.js(buildUrl(helperCDN, "_appendParamValues.js")),
-    load.js(buildUrl(helperCDN, "_checkParams.js")),
-    load.js(buildUrl(helperCDN, "_compareParams.js")),
-    load.js(buildUrl(helperCDN, "_joinParams.js")),
-    load.js(buildUrl(helperCDN, "_updateJoinedParams.js")),
-    load.js(buildUrl(helperCDN, "_updateParams.js")),
-    console.log("param helpers loaded @ " + today)
-]);
+        // load value functions
+        Promise.all([
+            load.js(buildUrl(helperCDN, "_getValue.js")),
+            load.js(buildUrl(helperCDN, "_initLinks.js")),
+            load.js(buildUrl(helperCDN, "_updateLink.js")),
+            console.log("value helpers loaded @ " + today)
+        ])).then(
 
-// load cookie functions
-var loadCookies = Promise.all([
-    load.js(buildUrl(helperCDN, "_getCookie.js")),
-    load.js(buildUrl(helperCDN, "_eraseCookie.js")),
-    load.js(buildUrl(helperCDN, "_logCookie.js")),
-    load.js(buildUrl(helperCDN, "_setCookie.js")),
-    load.js(buildUrl(helperCDN, "_updateCookie.js")),
-    load.js(buildUrl(helperCDN, "_syncCookies.js")),
-    console.log("cookie helpers loaded @ " + today)
-]);
+        // load partner id functions
+        Promise.all([
+            load.js(buildUrl(helperCDN, "_setPid.js")),
+            console.log("pid helpers loaded @ " + today)
+        ])).then(
 
-// put it all together
-var loadValues = Promise.all([
-    load.js(buildUrl(helperCDN, "_getValue.js")),
-    load.js(buildUrl(helperCDN, "_initLinks.js")),
-    load.js(buildUrl(helperCDN, "_updateLink.js")),
-    console.log("value helpers loaded @ " + today)
-]);
-
-// load pid function 
-var loadPid = Promise.all([
-    load.js(buildUrl(helperCDN, "_setPid.js")),
-    console.log("pid helpers loaded @ " + today)
-]);
-
-
-var loadIntegration = Promise.all([
-    load.js(buildUrl(distCDN, "marketlink.integration.0827.js")),
-    console.log("integration helpers loaded @ " + today)
-]);
-
-var loadBundle = function() {
-    loadHelpers.then(loadParams).then(loadCookies).then(loadValues).then(loadPid).then(loadIntegration);
-};
+        // load integration of functions into application
+        Promise.all([
+            load.js(buildUrl(distCDN, "marketlink.integration.0827.js")),
+            console.log("integration helpers loaded @ " + today)
+        ])
+    );
 
 loadBundle();
