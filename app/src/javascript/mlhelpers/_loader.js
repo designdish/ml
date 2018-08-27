7136d2d072a599bf204090eb9ffe946082bc8f7a
Bowie <bowie.leidy@gmail.com>
2018-08-27 12:30:46 -0400

    pushing in waitfor function

--- app/src/javascript/mlhelpers/_loader.js
+++ app/src/javascript/mlhelpers/_loader.js
@@ -1,36 +1,43 @@
 var load = (function() {
-    function _load(tag) {
-        return function(url) {
-            return new Promise(function(resolve, reject) {
-                var element = document.createElement(tag);
-                var parent = "body";
-                var attr = "src";
-                element.onload = function() {
-                    resolve(url);
-                };
-                element.onerror = function() {
-                    reject(url);
-                };
-                switch (tag) {
-                    case "script":
-                        element.async = true;
-                        break;
-                    case "link":
-                        element.type = "text/css";
-                        element.rel = "stylesheet";
-                        attr = "href";
-                        parent = "head";
-                }
+        function _load(tag) {
+            return function(url) {
+                return new Promise(function(resolve, reject) {
+                    var element = document.createElement(tag);
+                    var parent = "body";
+                    var attr = "src";
+                    element.onload = function() {
+                        resolve(url);
+                    };
+                    element.onerror = function() {
+                        waitFor(url);
+                    };
+                    switch (tag) {
+                        case "script":
+                            element.async = true;
+                            break;
+                        case "link":
+                            element.type = "text/css";
+                            element.rel = "stylesheet";
+                            attr = "href";
+                            parent = "head";
+                    }
 
-                element[attr] = url;
-                document[parent].appendChild(element);
-            });
+                    element[attr] = url;
+                    document[parent].appendChild(element);
+                });
+            };
+        }
+
+        return {
+            css: _load("link"),
+            js: _load("script"),
+            img: _load("img")
         };
-    }
-
-    return {
-        css: _load("link"),
-        js: _load("script"),
-        img: _load("img")
+    })(),
+    waitFor = function(url) {
+        return new Promise(function(resolve) {
+            setTimeout(function() {
+                resolve(url);
+            }, 250);
+        });
     };
-})();
