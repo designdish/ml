75fb4ed6aeb8635a375e41e6a19aa9d0e54d4fe6
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-29 02:51:42 -0400

    push update to keep current page parameters when the url isn't in the passed string

--- app/src/javascript/mlhelpers/_updateLink.js
+++ app/src/javascript/mlhelpers/_updateLink.js
@@ -1,16 +1,20 @@
 var updateLink = function(params, str, event) {
-    var link = event.target;
-    link = getClosest(link, "a");
-    linkURL = link.href;
+	var link = event.target;
+	link = getClosest(link, "a");
+	linkURL = link.href;
 
-    var currentPage = window.location.href;
+	var currentPage = window.location.href;
+	var currentParams = window.location.search;
+	if (linkURL.indexOf(str) != -1 && linkURL.indexOf("mailto") === -1) {
+		link.href = checkParams(linkURL, params);
 
-    if (linkURL.indexOf(str) != -1 && linkURL.indexOf("mailto") === -1) {
-        link.href = checkParams(linkURL, params);
-// if (joinParams != undefined) {
-//     link.href = joinParameters(linkURL, joinParams[0], joinParams[1]);
-// }
-    }
-    console.dir(link.href);
-    window.location = link.href;
+		// if (joinParams != undefined) {
+		//     link.href = joinParameters(linkURL, joinParams[0], joinParams[1]);
+		// }
+	} else if (linkURL.indexOf(str) === -1) {
+		window.location = link.href + currentParams;
+	} else {
+		console.dir(link.href);
+		window.location = link.href;
+	}
 };
