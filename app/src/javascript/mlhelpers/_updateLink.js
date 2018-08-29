eb91112f93c96e86835e36c37b9f9ff704131e9a
Bowie Leidy <bowie.leidy@gmail.com>
2018-08-29 03:16:24 -0400

    pushing some changes

--- app/src/javascript/mlhelpers/_updateLink.js
+++ app/src/javascript/mlhelpers/_updateLink.js
@@ -7,14 +7,10 @@ var updateLink = function(params, str, event) {
 	var currentParams = window.location.search;
 	if (linkURL.indexOf(str) != -1 && linkURL.indexOf("mailto") === -1) {
 		link.href = checkParams(linkURL, params);
+		window.location = link.href;
+		console.dir(link.href);
 
-		// if (joinParams != undefined) {
-		//     link.href = joinParameters(linkURL, joinParams[0], joinParams[1]);
-		// }
 	} else if (linkURL.indexOf(str) === -1) {
 		window.location = link.href + currentParams;
-	} 
-	console.dir(link.href);
-	window.location = link.href;
-
+	}
 };
