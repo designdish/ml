cd82284fa303d94ba92cf60a5169771269f4ca45
Bowie <bowie.leidy@gmail.com>
2018-08-31 11:15:17 -0400

    adding array of links to be excluded from manipulation

--- app/src/javascript/mlhelpers/_updateLink.js
+++ app/src/javascript/mlhelpers/_updateLink.js
@@ -1,18 +1,23 @@
-var updateLink = function(params, str, event) {
-	var link = event.target;
-	link = getClosest(link, "a");
-	linkURL = link.href;
+//  a list of excluded elements that should not be updated
+var excludedLinks = ['firstFlip'];
 
-	var currentPage = window.location.href;
-	var currentParams = window.location.search;
-	if (linkURL.indexOf(str) != -1 && linkURL.indexOf("mailto") === -1) {
-		link.href = checkParams(linkURL, params);
-		window.location = link.href;
-	} else if (linkURL.indexOf(str) === -1) {
-		if (currentDomain.indexOf(tvURL) != -1){
-			window.location = link.href + currentParams;
-		}else{
-			window.location = link.href;
-		}
-	}
+var updateLink = function(params, str, event) {
+    var link = event.target;
+    link = getClosest(link, "a");
+    linkURL = link.href;
+
+    var currentPage = window.location.href;
+    var currentParams = window.location.search;
+    if (excludedLinks.indexOf(link.id) === -1) {
+        if (linkURL.indexOf(str) != -1 && linkURL.indexOf("mailto") === -1) {
+            link.href = checkParams(linkURL, params);
+            window.location = link.href;
+        } else if (linkURL.indexOf(str) === -1) {
+            if (currentDomain.indexOf(tvURL) != -1) {
+                window.location = link.href + currentParams;
+            } else {
+                window.location = link.href;
+            }
+        }
+    }
 };
